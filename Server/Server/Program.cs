a634d0c6360eb47d576aece0f16df89de86fd722
Stefan Westermann <trommewestermann@hotmail.com>
2018-10-11 14:40:31 +0200

    Constraints for methods

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
+using Newtonsoft.Json;
 
 namespace Server
 {
     class Server
     {
+        static String[] allowedMethods = { "create", "read", "update", "delete", "echo" };
         static void Main(string[] args)
         {
-            String[] allowedMethods = { "create", "read", "update", "delete", "echo" };
-
             //server
             var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
             server.Start();
@@ -19,28 +20,81 @@ namespace Server
             while(true)
             {
                 var client = server.AcceptTcpClient();
-                Thread thread = new Thread(new ThreadStart(CreateStream));
-                thread.Start();
-            }
+
+                Console.WriteLine("Client found! :D");
+
+                HandleRequest(client);
+            } 
         }
 
-        static void CreateStream()
+        static void HandleRequest(TcpClient client)
+        {
+            var strm = client.GetStream();
+            var buffer = new Byte[client.ReceiveBufferSize];
+            //Only process request if any (constraint)
+            if (!strm.DataAvailable)
+            {
+                Console.WriteLine("OOOPS MOFO");
+                return;
+            }
+            var readCnt = strm.Read(buffer, 0, buffer.Length);
+            var msg = Encoding.UTF8.GetString(buffer, 0, readCnt);
+            
+            Request r = JsonConvert.DeserializeObject<Request>(msg);
+            Response res = CheckConstraints(r);
+            
+        }
+
+        static Response CheckConstraints(Request r)
+        {
+            Response res = new Response();
+                /*  CONSTRAINTS  */
+            //Missing method
+            if(r.Method == null)
+            {
+                AddToStatus(res, "Missing method!");
+            }
+            else
+            {
+                foreach (string a in allowedMethods)
+                {
+                    if (r.Method != a)
+                    {
+                        AddToStatus(res, "Illegal Method");
+                    }
+                }
+            }
+            return null;
+        }
+        static void CreateStream(TcpClient client)
         {
 
+        }
+
+        static void AddToStatus(Response r, string message)
+        {
+            if (r.Status == null)
+            {
+                r.Status += message;
+            }
+            else
+            {
+                r.Status += ", " + message;
+            }
         }
     }
 
     class Request
     {
-        string Method { get; set; }
-        string Path { get; set; }
-        string Date { get; set; }
-        string Body { get; set; }
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Date { get; set; }
+        public string Body { get; set; }
     }
     class Response
     {
-        string Status { get; set; }
-        string Body { get; set; }
+        public string Status { get; set; }
+        public string Body { get; set; }
     }
     class Category
     {
