8f5da2b912c650128176504688ea65780ea8c421
Stefan Westermann <trommewestermann@hotmail.com>
2018-10-11 13:25:07 +0200

    Starting on server

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Server
 {
@@ -6,7 +9,42 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String[] allowedMethods = { "create", "read", "update", "delete", "echo" };
+
+            //server
+            var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
+            server.Start();
+            Console.WriteLine("Server started ..");
+
+            while(true)
+            {
+                var client = server.AcceptTcpClient();
+                Thread thread = new Thread(new ThreadStart(CreateStream));
+                thread.Start();
+            }
         }
+
+        static void CreateStream()
+        {
+
+        }
+    }
+
+    class Request
+    {
+        string Method { get; set; }
+        string Path { get; set; }
+        string Date { get; set; }
+        string Body { get; set; }
+    }
+    class Response
+    {
+        string Status { get; set; }
+        string Body { get; set; }
+    }
+    class Category
+    {
+        string Cid { get; set; }
+        string Name { get; set; }
     }
 }
